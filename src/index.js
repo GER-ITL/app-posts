979352c3af2983df9d9f2a92890d7f26c160d9f1
GER-ITL <danteryzvelt@inbox.ru>
2023-11-22 16:54:06 +0300

    update app

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
-import state, { subscribe } from "./redux/state";
+import store from "./redux/state";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { addPost, removePost, updateNewPostText } from "./redux/state";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-let reranderEntireTree = (state) => {
+let reranderEntireTree = () => {
   root.render(
     <BrowserRouter>
       <React.StrictMode>
         <App
-          state={state}
-          addPost={addPost}
-          removePost={removePost}
-          updateNewPostText={updateNewPostText}
+          state={store.getState()}
+          addPost={store.addPost.bind(store)}
+          removePost={store.removePost.bind(store)}
+          updateNewPostText={store.updateNewPostText.bind(store)}
         />
       </React.StrictMode>
     </BrowserRouter>
   );
 };
 
-reranderEntireTree(state);
-subscribe(reranderEntireTree);
+reranderEntireTree(store.getState());
+store.subscribe(reranderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,140 +1,144 @@
-let reranderEntireTree = () => {
-  console.log("State change");
+let store = {
+  _state: {
+    profilePage: {
+      postsData: [
+        {
+          id: 1,
+          img: "https://img.freepik.com/premium-vector/anonymous-hooded-avatar-hidden-user-incognito-hacker-isolated-vector-illustration_619989-1263.jpg",
+          mess: "Hello bro",
+          count: 10,
+        },
+        {
+          id: 2,
+          img: "https://img.freepik.com/premium-vector/anonymous-hooded-avatar-hidden-user-incognito-hacker-isolated-vector-illustration_619989-1263.jpg",
+          mess: "How are you?",
+          count: 20,
+        },
+      ],
+      profileData: [
+        {
+          name: "Dante R.",
+          dataBirth: "8 january",
+          citi: "Moscow",
+          education: "BSU'11",
+          img: "https://img.freepik.com/premium-vector/anonymous-hooded-avatar-hidden-user-incognito-hacker-isolated-vector-illustration_619989-1263.jpg",
+        },
+      ],
+      newPostText: "",
+    },
+    messagesPage: {
+      dialogsData: [
+        {
+          id: 1,
+          name: "Alex",
+        },
+        {
+          id: 2,
+          name: "Jame",
+        },
+        {
+          id: 3,
+          name: "Vinsent",
+        },
+        {
+          id: 4,
+          name: "Jack",
+        },
+        {
+          id: 5,
+          name: "Johnny",
+        },
+      ],
+      messagesData: [
+        {
+          id: 1,
+          message: "mess 1",
+        },
+        {
+          id: 2,
+          message: "mess 2",
+        },
+        {
+          id: 3,
+          message: "mess 3",
+        },
+        {
+          id: 4,
+          message: "mess 4",
+        },
+        {
+          id: 5,
+          message: "mess 5",
+        },
+      ],
+    },
+    friendsPage: {
+      friends: [
+        {
+          id: 1,
+          name: "Alex L.",
+          img: "https://i.pinimg.com/736x/2e/2e/21/2e2e2125ee53807c2d77b34773f84b5c.jpg",
+          status: "online",
+        },
+        {
+          id: 2,
+          name: "Jack S.",
+          img: "https://cs14.pikabu.ru/post_img/big/2023/02/13/8/1676296367166243426.png",
+          status: "offline",
+        },
+        {
+          id: 3,
+          name: "Max J.",
+          img: "https://i.pinimg.com/736x/f5/27/41/f52741fb62bf1d821948a49204406bdc.jpg",
+          status: "offline",
+        },
+        {
+          id: 4,
+          name: "Margo N.",
+          img: "https://sunmag.me/wp-content/uploads/2019/11/sunmag-001-small-avatar.png",
+          status: "online",
+        },
+      ],
+    },
+    newsPage: {
+      news: [
+        { id: 1, label: "news 1" },
+        { id: 2, label: "news 2" },
+        { id: 3, label: "news 3" },
+        { id: 4, label: "news 4" },
+      ],
+    },
+  },
+  getState() {
+    return this._state;
+  },
+  _reranderEntireTree() {
+    console.log("State change");
+  },
+  addPost() {
+    let newPost = {
+      id: 6,
+      img: "https://img.freepik.com/premium-vector/anonymous-hooded-avatar-hidden-user-incognito-hacker-isolated-vector-illustration_619989-1263.jpg",
+      mess: this._state.profilePage.newPostText,
+      count: 0,
+    };
+    this._state.profilePage.postsData.push(newPost);
+    this._state.profilePage.newPostText = "";
+    this._reranderEntireTree(this._state);
+  },
+
+  updateNewPostText(messPost) {
+    this._state.profilePage.newPostText = messPost;
+    this._reranderEntireTree(this._state);
+  },
+
+  removePost() {
+    this._state.profilePage.postsData.pop();
+    this._reranderEntireTree(this._state);
+  },
+  subscribe(observer) {
+    this._reranderEntireTree = observer;
+  },
 };
 
-let state = {
-  profilePage: {
-    postsData: [
-      {
-        id: 1,
-        img: "https://img.freepik.com/premium-vector/anonymous-hooded-avatar-hidden-user-incognito-hacker-isolated-vector-illustration_619989-1263.jpg",
-        mess: "Hello bro",
-        count: 10,
-      },
-      {
-        id: 2,
-        img: "https://img.freepik.com/premium-vector/anonymous-hooded-avatar-hidden-user-incognito-hacker-isolated-vector-illustration_619989-1263.jpg",
-        mess: "How are you?",
-        count: 20,
-      },
-    ],
-    profileData: [
-      {
-        name: "Dante R.",
-        dataBirth: "8 january",
-        citi: "Moscow",
-        education: "BSU'11",
-        img: "https://img.freepik.com/premium-vector/anonymous-hooded-avatar-hidden-user-incognito-hacker-isolated-vector-illustration_619989-1263.jpg",
-      },
-    ],
-    newPostText: "",
-  },
-  messagesPage: {
-    dialogsData: [
-      {
-        id: 1,
-        name: "Alex",
-      },
-      {
-        id: 2,
-        name: "Jame",
-      },
-      {
-        id: 3,
-        name: "Vinsent",
-      },
-      {
-        id: 4,
-        name: "Jack",
-      },
-      {
-        id: 5,
-        name: "Johnny",
-      },
-    ],
-    messagesData: [
-      {
-        id: 1,
-        message: "mess 1",
-      },
-      {
-        id: 2,
-        message: "mess 2",
-      },
-      {
-        id: 3,
-        message: "mess 3",
-      },
-      {
-        id: 4,
-        message: "mess 4",
-      },
-      {
-        id: 5,
-        message: "mess 5",
-      },
-    ],
-  },
-  friendsPage: {
-    friends: [
-      {
-        id: 1,
-        name: "Alex L.",
-        img: "https://i.pinimg.com/736x/2e/2e/21/2e2e2125ee53807c2d77b34773f84b5c.jpg",
-        status: "online",
-      },
-      {
-        id: 2,
-        name: "Jack S.",
-        img: "https://cs14.pikabu.ru/post_img/big/2023/02/13/8/1676296367166243426.png",
-        status: "offline",
-      },
-      {
-        id: 3,
-        name: "Max J.",
-        img: "https://i.pinimg.com/736x/f5/27/41/f52741fb62bf1d821948a49204406bdc.jpg",
-        status: "offline",
-      },
-      {
-        id: 4,
-        name: "Margo N.",
-        img: "https://sunmag.me/wp-content/uploads/2019/11/sunmag-001-small-avatar.png",
-        status: "online",
-      },
-    ],
-  },
-  newsPage: {
-    news: [
-      { id: 1, label: "news 1" },
-      { id: 2, label: "news 2" },
-      { id: 3, label: "news 3" },
-      { id: 4, label: "news 4" },
-    ],
-  },
-};
-
-export const addPost = () => {
-  let newPost = {
-    id: 6,
-    img: "https://img.freepik.com/premium-vector/anonymous-hooded-avatar-hidden-user-incognito-hacker-isolated-vector-illustration_619989-1263.jpg",
-    mess: state.profilePage.newPostText,
-    count: 0,
-  };
-  state.profilePage.postsData.push(newPost);
-  state.profilePage.newPostText = "";
-  reranderEntireTree(state);
-};
-
-export const updateNewPostText = (messPost) => {
-  state.profilePage.newPostText = messPost;
-  reranderEntireTree(state);
-};
-
-export const removePost = () => {
-  state.profilePage.postsData.pop();
-  reranderEntireTree(state);
-};
-export const subscribe = (observer) => {
-  reranderEntireTree = observer;
-};
-export default state;
+export default store;
