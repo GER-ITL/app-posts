539aa46469aba81eeccb84921a12714d164472c8
GER-ITL <danteryzvelt@inbox.ru>
2023-12-25 08:26:13 +0300

    update family

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import styles from './Header.module.scss'
-import SideImg from './SideImg'
 const Header = (props) => {
   return (
     <div className={styles.header}>
@@ -10,9 +9,8 @@ const Header = (props) => {
       <img src="https://cdn-icons-png.flaticon.com/512/1390/1390385.png" alt="" />
       </div>
       <div className={styles.label}>
-      <h1>www.social_network</h1>
+      <h1>www.Social_Network</h1>
       </div>
-      <SideImg/>
       <div className={styles.loginBlock}>
         
         <div>{props.isAuth ? props.login : <NavLink to={"/login"}>LogIn</NavLink> }</div>
